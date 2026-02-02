e0aec1676e2796861dfde18d6ed78c7d85aa51ee
Artur Pelo <artur.pelo@gmail.com>
2026-02-02 19:32:06 +0100

    Dodanie rozdziały. Wpisano przykładow dane do rozdziałów. We wstępie dodano modele obiektów do umieszczenia w pracy.

--- start_w_Dockert.bat
+++ start_w_Dockert.bat
@@ -8,27 +8,39 @@ if "%1" neq "new" (
 
 :: --- Faktyczne działanie w nowym CMD ---
 cls
+:: Ustawienie zmiennej ESC dla kolorów ANSI
+for /f %%e in ('echo prompt $E ^| cmd') do set "ESC=%%e"
 echo __________________________________________________________________
 echo [1/3] Czyszczenie plikow wyjsciowych w katalogu glownym...
 
-del /q *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.out *.run.xml *.synctex.gz *.toc *.lof *.lot *.nav *.snm *.vrb 2>nul
-del /q main.pdf 2>nul
+del /q *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.out *.run.xml *.synctex.gz *.toc *.lof *.lot *.nav *.snm *.vrb >nul 2>nul
+del /q main.pdf >nul 2>nul
 echo __________________________________________________________________
 
 echo [2/3] Kompilacja przez Docker (latexmk)...
+:: Sprawdzenie dostepnosci Dockera
+docker info >nul 2>nul
+if errorlevel 1 (
+    echo __________________________________________________________________
+    echo %ESC%[31m[BLAD] Docker nie jest uruchomiony lub niedostepny.%ESC%[0m
+    echo %ESC%[31m       Uruchom Docker Desktop i sprobuj ponownie.%ESC%[0m
+    goto :after_compile
+)
 :: Wyjscie i pliki pomocnicze w katalogu glownym
 docker run --rm -v "%cd%:/workdir" texlive/texlive sh -c "latexmk -pdf -interaction=nonstopmode -file-line-error main.tex"
 
 :: Drugi przebieg dla spisu tresci, rysunkow i tabel
 docker run --rm -v "%cd%:/workdir" texlive/texlive sh -c "latexmk -pdf -interaction=nonstopmode -file-line-error main.tex"
 
+:after_compile
+
 echo __________________________________________________________________
 echo [3/3] Sprawdzanie gotowego PDF w katalogu glownym...
 
-:: Ustawienie zmiennej ESC dla kolorów ANSI
-for /f %%e in ('echo prompt $E ^| cmd') do set "ESC=%%e"
+set "PDF_OK=0"
 
 if exist main.pdf (
+    set "PDF_OK=1"
     :: Ramka sukcesu
     echo %ESC%[32m┌───────────────────────────────────────────────────────────────┐%ESC%[0m
     echo %ESC%[32m^|     Sukces! Plik PDF znajduje sie w katalogu glownym.          ^|%ESC%[0m
@@ -38,8 +50,6 @@ if exist main.pdf (
     echo %ESC%[31m┌───────────────────────────────────────────────────────────────┐%ESC%[0m
     echo %ESC%[31m^|   Blad: Nie znaleziono pliku PDF. Sprawdz logi w katalogu.    ^|%ESC%[0m
     echo %ESC%[31m└───────────────────────────────────────────────────────────────┘%ESC%[0m
-    pause
-    exit /b
 )
 
 :: Dodatkowe info
@@ -49,9 +59,13 @@ echo %ESC%[90m^|       Copyright © 2026 Artur Pelo. All rights reserved.       
 echo %ESC%[90m└───────────────────────────────────────────────────────────────┘%ESC%[0m
 
 :: Kasowanie plikow tymczasowych
-del /q *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.lol *.out *.run.xml *.synctex.gz *.toc *.lof *.lot *.nav *.snm *.vrb 2>nul
-del /q main.bbl main.bcf main.run.xml 2>nul
+del /q *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.lol *.out *.run.xml *.synctex.gz *.toc *.lof *.lot *.nav *.snm *.vrb >nul 2>nul
+del /q main.bbl main.bcf main.run.xml >nul 2>nul
 
 echo.
 pause
-exit /b
+if "%PDF_OK%"=="1" (
+    exit /b 0
+) else (
+    exit /b 1
+)
